45859f16a5a905d8f0485144d68b8dde018bc93d
IA-LINK <jyc.kom@gmail.com>
2023-08-30 06:14:45 -0700

    handle_print file on repo

--- handle_print.c
+++ handle_print.c
@@ -4,8 +4,8 @@
 * handle_print - Prints an argument based on its type
 * @fmt: Formatted string in which to print the arguments.
 * @list: List of arguments to be printed.
-* @ind: ind.
-* @buffer: Buffer array to handle print.
+* @knd: knd.
+I* @buffer: Buffer array to handle print.
 * @flags: Calculates active flags
 * @width: get width.
 * @precision: Precision specification
@@ -13,7 +13,7 @@
 * Return: 1 or 2;
 */
 
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+int handle_print(const char *fmt, int *knd, va_list list, char buffer[],
 int flags, int width, int precision, int size)
 {
 int k, unknow_len = 0, printed_chars = -1;
@@ -25,25 +25,25 @@ fmt_t fmt_types[] = {
 {'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 };
 for (k = 0; fmt_types[k].fmt != '\0'; k++)
-if (fmt[*ind] == fmt_types[k].fmt)
+if (fmt[*knd] == fmt_types[k].fmt)
 return (fmt_types[k].fn(list, buffer, flags, width, precision, size));
 if (fmt_types[k].fmt == '\0')
 {
-if (fmt[*ind] == '\0')
+if (fmt[*knd] == '\0')
 return (-1);
 unknow_len += write(1, "%%", 1);
-if (fmt[*ind - 1] == ' ')
+if (fmt[*knd - 1] == ' ')
 unknow_len += write(1, " ", 1);
 else if (width)
 {
---(*ind);
-while (fmt[*ind] != ' ' && fmt[*ind] != '%')
---(*ind);
-if (fmt[*ind] == ' ')
---(*ind);
+--(*knd);
+while (fmt[*knd] != ' ' && fmt[*knd] != '%')
+--(*knd);
+if (fmt[*knd] == ' ')
+--(*knd);
 return (1);
 }
-unknow_len += write(1, &fmt[*ind], 1);
+unknow_len += write(1, &fmt[*knd], 1);
 return (unknow_len);
 }
 return (printed_chars);
--- main.h
+++ main.h
@@ -40,7 +40,7 @@ int (*fn)(va_list, char[], int, int, int, int);
 
 typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
+int handle_print(const char *fmt, int *k,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** Functions ******************/
